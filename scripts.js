f9cb37c49ec7651ae883577461e92f3b24e5fb83
Ashish <ashish.blackhawk@gmail.com>
2021-01-02 00:02:22 +0530

    update-1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
 AOS.init();
+let about = document.querySelector("#about");
+let services = document.querySelector("#services");
+let cases = document.querySelector("#cases");
+let blog = document.querySelector("#blog");
+let contact = document.querySelector("#contact");
+let home = document.querySelector("#home");
+let logo = document.querySelector("#logo");
+let down = document.querySelector("#down-pointer");
+
+about.addEventListener("click", () => {
+  Jump("#about-section");
+});
+cases.addEventListener("click", () => {
+  Jump("#cases-section");
+});
+blog.addEventListener("click", () => {
+  Jump("#blog-section");
+});
+contact.addEventListener("click", () => {
+  Jump("#contact-section");
+});
+home.addEventListener("click", () => {
+  Jump("#hero-text");
+});
+services.addEventListener("click", () => {
+  Jump("#services-section");
+});
+logo.addEventListener("click", () => {
+  Jump("#hero-text");
+});
+down.addEventListener("click", () => {
+  Jump("#services-section");
+});
